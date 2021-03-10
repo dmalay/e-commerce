1cc8d5a3c414dc4397f7e7e9dfbd0537fc34039f
dmalay <dmalay1228@gmail.com>
2021-03-10 23:48:03 +0300

    implemented adding/removing to cart

--- client/components/common/card.js
+++ client/components/common/card.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { changeCart } from '../../redux/reducers/products'
 
 const Card = (props) => {
+  const dispatch = useDispatch()
+
   const { list } = props
   const currency = useSelector((s) => s.products.currency)
   const rate = useSelector((s) => s.products.rates[currency])
   const priceRecalc = typeof rate === 'undefined' ? list.price : list.price * rate
+  const quantity = useSelector((s) => s.products.cart[list.id])
+
+  const onClickAdd = () => {
+    dispatch(changeCart(list.id, quantity ? quantity + 1 : 1))
+  }
+  const onClickRemove = () => {
+    dispatch(changeCart(list.id, quantity > 1 ? quantity - 1: 0))
+  }
+  const onClickDelete = () => {
+    dispatch(changeCart(list.id, 0))
+  }
 
   return (
     <div>
@@ -14,10 +29,11 @@ const Card = (props) => {
         <div className="card__price">price:{priceRecalc.toFixed(2)}</div>
         <div className="currency">currency{currency}</div>
         <div className="card__title">title{list.title}</div>
-        <div className="card__product-amount">amnt</div>
+        <div className="card__product-amount">amnt{quantity}</div>
       </div>
-      <button type="button" >Minus Button</button>
-      <button type="button" >Plus Button</button>
+      <button type="button" onClick={onClickRemove}>Minus Button</button>
+      <button type="button" onClick={onClickDelete}>Minus Button</button>
+      <button type="button" onClick={onClickAdd}>Plus Button</button>
     </div>
   )
 }
--- client/redux/reducers/products.js
+++ client/redux/reducers/products.js
@@ -4,13 +4,14 @@ import sortData from './products.util'
 const GET_PRODUCTS = 'GET_PRODUCTS'
 const SET_CURRENCY = 'SET_CURRENCY'
 const SET_SORT = 'SET_SORT'
+const CHANGE_CART = 'CHANGE_CART'
 
 const initialState = {
   productCards: [],
   currency: 'EUR',
   rates: {},
   method: {},
-  basket: {}
+  cart: {}
 }
 
 export default (state = initialState, action) => {
@@ -28,15 +29,21 @@ export default (state = initialState, action) => {
         ...state,
         currency,
         rates
-
       }
     }
     case SET_SORT: {
-      const { productCards, method} = action
+      const { productCards, method } = action
       return {
         ...state,
         productCards,
         method
+      }
+    }
+    case CHANGE_CART: {
+      const { cart } = action
+      return {
+        ...state,
+        cart
       }
     }
     default:
@@ -61,12 +68,23 @@ export function setCurrency(currency) {
 }
 
 export function setSortMethod(method) {
-
   return (dispatch, getState) => {
     const store = getState()
-    const {productCards } = store.products
+    const { productCards } = store.products
     const sortMethod = JSON.parse(method)
     const sortedCards = sortData(productCards, sortMethod)
     dispatch({ type: SET_SORT, productCards: sortedCards, method: sortMethod })
+  }
+}
+
+export function changeCart(id, quantity) {
+  return (dispatch, getState) => {
+    const store = getState()
+    const { cart } = store.products
+    const addingData = { ...cart, [id]: quantity }
+    if(!quantity) {
+      delete addingData[id]
+    }
+    dispatch({ type: CHANGE_CART, cart: addingData })
   }
 }
